9d4669ce5b389bf2f2f383d91a06693cb3feb4c9
Gengorou-C <90015823+Gengorou-C@users.noreply.github.com>
2023-12-09 19:57:30 +0900

    宛名面に関する変更 * フォントの種類とサイズ * 数字を縦向きに * 「-」(U+002d)を「‐」(U+2010)に置換

--- src/personalData.cpp
+++ src/personalData.cpp
@@ -26,6 +26,7 @@ int personalData::setAddress(std::string address){
   if(address.empty()){
     address = "";
   }
+  address = std::regex_replace(address, std::regex(R"(-)"), "‐");
   this->address = address;
   return 0;
 }
